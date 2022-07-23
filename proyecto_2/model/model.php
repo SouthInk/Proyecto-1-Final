400582f6ba0aef02a18294bad3124bdbc0fbf265
SouthInk <46875913+SouthInk@users.noreply.github.com>
2022-07-22 18:41:39 -0600

    Add files via upload

--- proyecto_2/model/model.php
+++ proyecto_2/model/model.php
@@ -19,13 +19,24 @@ private static $instance = null;
 
     }
 //solo le interesa tener los recursos, no conectarse a la base de datos
-    function val_login($usu,$pass){
+    function val_login($usu,$pass,$logintype){
         $this->ins_conexion = conexion::getInstance();
         $this->obj_conexion = $this->ins_conexion->conectar();
-        $sql = "SELECT id_alumno,nivel_id,login_alumno,clave_alumno,nombre_alumno,apellidos_alumno FROM alumno";
-        $sql .= " WHERE login_alumno='$usu' AND clave_alumno=md5('$pass')";
+        
+        if ($logintype == "alumno") {
+          $sql = "SELECT id_alumno,nivel_id,login_alumno,clave_alumno,nombre_alumno,apellidos_alumno FROM alumno";
+          $sql .= " WHERE login_alumno='$usu' AND clave_alumno=md5('$pass')";
+        }
+        else if($logintype == "admin"){
+          $sql = "SELECT id_admin,login_admin,clave_admin,email_admin FROM administrador";
+          $sql .= " WHERE login_admin='$usu' AND clave_admin=md5('$pass')";
+          
+        }else{
+          $sql = "SELECT id_profesor,login_profesor,clave_profesor,nombre_profesor,apellidos_profesor,email_profesor,especialidad_prfoesor FROM profesor";
+          $sql .= " WHERE login_alumno='$usu' AND clave_alumno=md5('$pass')";
+        }
+        
         $rs = $this->obj_conexion->query($sql); 
-       
         $this->ins_conexion->desconectar();
         return $rs;
     }
